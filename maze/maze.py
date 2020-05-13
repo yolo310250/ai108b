6e10a16c3816bf40903fee718b8a353caa916a46
yolo310250 <monster113080@gmail.com>
2020-05-13 15:13:21 +0800

    yolo310250

---
+++ maze/maze.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def matrixPrint(m):
+    i = 0
+    j = 0
+    for i in range(6):
+        for j in range(8):
+            print(m[i][j],end='')
+        print()
+
+def findPath(m,x,y):
+    print("=========================")
+    print("x=", x, " y=", y,)
+    matrixPrint(m)
+    if x>=6 or y>=8 :return False  
+    if m[x][y] == '*' :return False  
+    if m[x][y] == '+' :return False  
+    if m[x][y] == ' ' :m[x][y]='.'  
+    if m[x][y] == '.'  and (x == 5  or  y==7) : 
+        return True
+    if y<7 and m[x][y+1]==' ' : 
+        m[x][y]='.'
+        if findPath(m, x,y+1) :return True #向右
+    if x<5 and m[x+1][y]==' ' :
+        m[x][y]='.'
+        if findPath(m, x+1,y) :return True #往上
+    if y>0 and m[x][y-1]==' ':
+        m[x][y]=''
+        if findPath(m, x,y-1) :return True #往左
+    if x>0 and m[x-1][y]==' ' :
+        m[x][y]='.'
+        if findPath(m, x-1,y) :return True #往下
+    m[x][y]=' '
+    return False
+
+
+
+m=(['*','*','*','*','*','*','*','*']
+    ,[' ',' ','*','*','*','*','*','*']
+    ,['*',' ',' ',' ',' ',' ',' ','*']
+    ,['*','*','*','*',' ','*','*','*']
+    ,['*','*',' ',' ',' ',' ','*','*']
+    ,['*','*','*',' ','*','*','*','*'])
+findPath(m, 2, 3)
+print("=========================")
+matrixPrint(m)
